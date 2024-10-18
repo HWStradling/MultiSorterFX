58bc2c604eed3d9fd6799fe9f84c738a998dffd8
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-10-18 21:49:21 +0100

    renamed hello scene to splash scene

--- src/main/java/com/frontend/multisorterfx/SplashController.java
+++ src/main/java/com/frontend/multisorterfx/SplashController.java
@@ -3,7 +3,7 @@ package com.frontend.multisorterfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class SplashController {
     @FXML
     private Label welcomeText;
 
