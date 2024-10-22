0741c42b0b74b7cc325e2db7178a4fa92ca093c2
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-10-22 10:58:23 +0100

    created and added logo to splash scene

--- src/main/java/com/frontend/multisorterfx/SplashController.java
+++ src/main/java/com/frontend/multisorterfx/SplashController.java
@@ -2,8 +2,11 @@ package com.frontend.multisorterfx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 public class SplashController {
     @FXML
     private Label splashText;
+    @FXML
+    private ImageView splashImage;
 }
