70a6333b95a52abb1080b27a2ee00e02643be08b
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-11-05 22:51:29 +0000

    completed basic fronted implementation
    Fixed scene sizing bug, completed basic frontend implementation calling the backend and displaying the results.

--- src/main/java/com/frontend/multisorterfx/SortController.java
+++ src/main/java/com/frontend/multisorterfx/SortController.java
@@ -1,41 +1,168 @@
 package com.frontend.multisorterfx;
+
+import com.backend.multisorterfx.*;
+import com.backend.multisorterfx.statics.UnsortedArray;
 import com.frontend.multisorterfx.statics.SceneSwitcher;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Screen;
-import javafx.stage.Stage;
+
+import java.security.InvalidParameterException;
+import java.util.Arrays;
 
 
 public class SortController {
     // Container Elements
-    @FXML private HBox rootContainer;
-    @FXML private VBox bottomDisplayPane;
-    @FXML private VBox buttonPane;
-    @FXML private VBox displayPaneContainer;
-    @FXML private VBox topDisplayPane;
+    @FXML
+    private HBox rootContainer;
+    @FXML
+    private VBox bottomDisplayPane;
+    @FXML
+    private VBox buttonPane;
+    @FXML
+    private VBox displayPaneContainer;
+    @FXML
+    private VBox topDisplayPane;
 
     // Sorting Buttons
-    @FXML private Button bubbleSortButton;
-    @FXML private Button insertionSortButton;
-    @FXML private Button mergeSortButton;
-    @FXML private Button quickSortButton;
-    @FXML private Button selectionSortButton;
+    @FXML
+    private Button bubbleSortButton;
+    @FXML
+    private Button insertionSortButton;
+    @FXML
+    private Button mergeSortButton;
+    @FXML
+    private Button quickSortButton;
+    @FXML
+    private Button selectionSortButton;
+
+    // Array Generation Elements and Variables
+    @FXML
+    private Button generateButton;
+    @FXML
+    private Slider arrayLengthSlider;
+    @FXML
+    private Spinner<Integer> integerRange;
+    @FXML
+    private Label unsortedArrayLabel;
+    private int[] unsortedArray = new int[10];
+
+    //Sorted Array Elements
+    @FXML
+    private Label sortedArrayLabel;
+    @FXML
+    private Label algorithmNameLabel;
+    private int[] sortingArray = new int[10];
 
     @FXML
-    public void initialize(){
-        rootContainer.setPrefWidth(SceneSwitcher.getStage().getWidth());
-        System.out.println(SceneSwitcher.getStage().getWidth());
-        rootContainer.setPrefHeight(SceneSwitcher.getStage().getWidth());
-        rootContainer.setStyle("-fx-background-color: green;"); // for testing
-        double prefWidth = rootContainer.getPrefWidth();
-        displayPaneContainer.setPrefWidth( prefWidth/ 2);
-        System.out.println( prefWidth/ 2);
+    public void initialize() {
+        Platform.runLater(() -> {
+            rootContainer.setPrefWidth(SceneSwitcher.getStage().getWidth());
+            rootContainer.setPrefHeight(SceneSwitcher.getStage().getHeight());
+            rootContainer.setStyle("-fx-background-color: green;"); // for testing
 
-        displayPaneContainer.setStyle("-fx-background-color: yellow;");
+            displayPaneContainer.setPrefWidth(rootContainer.getPrefWidth() / 2);
+            displayPaneContainer.setStyle("-fx-background-color: yellow;"); // for testing
+
+            topDisplayPane.setPrefWidth(displayPaneContainer.getPrefWidth());
+            topDisplayPane.setStyle("-fx-background-color: blue;"); // for testing
+
+            bottomDisplayPane.setPrefWidth(displayPaneContainer.getPrefWidth());
+            bottomDisplayPane.setStyle("-fx-background-color: red"); // for testing
+
+            arrayLengthSlider.setValue(1);
+            arrayLengthSlider.setStyle(""); //todo how to change tick color
+
+            IntegerSpinnerValueFactory valueFactory = new IntegerSpinnerValueFactory(1, 100, 50); // min, max, initial value
+            integerRange.setValueFactory(valueFactory);
+
+            displayUnsortedArray();
+        });
     }
 
+    public void onQuicksortButtonAction(ActionEvent actionEvent) {
+        sortCaller("quick");
+    }
+    public void onMergesortButtonAction(ActionEvent actionEvent) {
+        sortCaller("merge");
+    }
 
+    public void onInsertionsortButtonAction(ActionEvent actionEvent) {
+        sortCaller("insertion");
+    }
+
+    public void onBubblesortButtonAction(ActionEvent actionEvent) {
+        sortCaller("bubble");
+    }
+
+    public void onSelectionsortButtonAction(ActionEvent actionEvent) {
+        sortCaller("selection");
+    }
+
+    public void onGenerateButtonAction(ActionEvent actionEvent) {
+        //todo push to notification stack/make notification stack
+        try {
+            unsortedArray = UnsortedArray.getUnsortedArray((int) arrayLengthSlider.getValue(), integerRange.getValue());
+        } catch (Exception e) {
+            System.out.print("slider value cast failed or get int range, using default array length of 5");
+            unsortedArray = UnsortedArray.getUnsortedArray(10, 10);
+        }
+        displayUnsortedArray();
+        resetSortingArray();
+    }
+
+    private void sortCaller(String x){
+        System.out.println(x);
+        resetSortingArray();
+        SortingAlgorithm sortingAlgorythm;
+        switch (x){
+            case "quick":
+                sortingAlgorythm = new Quicksort();
+                algorithmNameLabel.setText("QuickSort:");
+                break;
+            case "merge":
+                sortingAlgorythm = new Mergesort();
+                algorithmNameLabel.setText("MergeSort:");
+                break;
+            case "insertion":
+                sortingAlgorythm = new InsertionSort();
+                algorithmNameLabel.setText("InsertionSort:");
+                break;
+            case "bubble":
+                sortingAlgorythm = new BubbleSort();
+                algorithmNameLabel.setText("BubbleSort:");
+                break;
+            case "selection":
+                sortingAlgorythm = new SelectionSort();
+                algorithmNameLabel.setText("SelectionSort:");
+                break;
+            default:
+                sortingAlgorythm = new InsertionSort();
+                algorithmNameLabel.setText("Error Default,InsertionSort:");
+                System.out.print("algorithm switcher defaulting to insertion sort, parameter error");
+        }
+        sortingAlgorythm.sort(sortingArray);
+        displaySortingArray();
+
+    }
+
+    private void resetSortingArray() {
+        sortingArray = Arrays.copyOf(unsortedArray, unsortedArray.length); // clones unsorted to prevent modification of unsorted array.
+    }
+
+    private void displayUnsortedArray() {
+        unsortedArrayLabel.setText(Arrays.toString(unsortedArray));
+    }
+
+    private void displaySortingArray(){
+        //todo make a teaching sort operations stack for bottom pane,
+        sortedArrayLabel.setText(Arrays.toString(sortingArray));
+    }
 }
--- src/main/java/com/frontend/multisorterfx/statics/SceneSwitcher.java
+++ src/main/java/com/frontend/multisorterfx/statics/SceneSwitcher.java
@@ -10,25 +10,18 @@ import java.io.IOException;
 
 public class SceneSwitcher {
 
-    private static double screenWidth;
-    private static double screenHeight;
+    private static final double screenWidth = Screen.getPrimary().getBounds().getWidth();
+    private static final double screenHeight = Screen.getPrimary().getBounds().getHeight();
 
     public static Stage stage;
 
 
-    public SceneSwitcher() {
-        setScreenWidth();
-        setScreenHeight();
-    }
     public static void setScene(String relativeFXMLRoute, int percentageWidth, int percentageHeight){
         try {
-//            setScreenWidth(); todo sort issue with stage size not correct when starting scene, stage size has percentage height applied twice if these lines called
-//            setScreenHeight();
             FXMLLoader fxmlLoader = new FXMLLoader(SplashScene.class.getResource(relativeFXMLRoute));
-            Scene scene = new Scene(fxmlLoader.load(), screenWidth * percentageWidth/100, screenHeight * percentageHeight/100 );
+            Scene scene = new Scene(fxmlLoader.load(), (getScreenWidth() * percentageWidth) /100, (getScreenHeight() * percentageHeight) /100 );
             stage.centerOnScreen(); //todo fix stage not centered on screen
             stage.setScene(scene);
-
             stage.show();
         } catch (IOException e) {
             System.out.println("error setting stage");
@@ -40,16 +33,8 @@ public class SceneSwitcher {
         return screenWidth;
     }
 
-    public static void setScreenWidth() {
-        SceneSwitcher.screenWidth = Screen.getPrimary().getBounds().getWidth();
-    }
-
     public static double getScreenHeight() {
         return screenHeight;
-    }
-
-    public static void setScreenHeight() {
-        SceneSwitcher.screenHeight = Screen.getPrimary().getBounds().getHeight();
     }
 
     public static Stage getStage() {
