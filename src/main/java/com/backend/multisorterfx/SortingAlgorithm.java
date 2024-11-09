592a131252f49ab5043189f1f19be8c10024d1c4
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-11-09 17:20:35 +0000

    Implemented Merge sort, changed access modifiers for some methods in abstract superclass SortingAlgorithm to protected.

--- src/main/java/com/backend/multisorterfx/SortingAlgorithm.java
+++ src/main/java/com/backend/multisorterfx/SortingAlgorithm.java
@@ -3,15 +3,20 @@ package com.backend.multisorterfx;
 import java.util.Arrays;
 
 public abstract class SortingAlgorithm {
-    
-    int[] sortingList;
+
+    protected int[] sortingList;
+
     public abstract void sort(int[] sortingList);
-    public void print(){
-        System.out.println(this.getClass().getSimpleName() +  Arrays.toString(sortingList));
+
+    protected void print() {
+        System.out.println(this.getClass().getSimpleName() + Arrays.toString(sortingList));
     }
-    public void swap(int swapIndex1, int swapIndex2) {
+
+    protected void swap(int swapIndex1, int swapIndex2) {
         int temp = sortingList[swapIndex1];
         sortingList[swapIndex1] = sortingList[swapIndex2];
         sortingList[swapIndex2] = temp;
     }
 }
+
+
