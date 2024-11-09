4d29e70aa23fbdcc6dee0ea7e993fec2626149df
HWStradling <99242469+HWStradling@users.noreply.github.com>
2024-11-09 00:28:15 +0000

    Implemented selection sort algorithm

--- src/main/java/com/backend/multisorterfx/SelectionSort.java
+++ src/main/java/com/backend/multisorterfx/SelectionSort.java
@@ -3,6 +3,25 @@ package com.backend.multisorterfx;
 public class SelectionSort extends SortingAlgorithm{
     @Override
     public void sort(int[] sortingList) {
+        this.sortingList = sortingList;
+        print();
+        selectionsort();
+        print();
+    }
 
+    private void selectionsort() {
+
+
+        for (int x = 0; x < sortingList.length - 1; x++) {
+            int currentMinimum = x;
+            for (int i = x + 1; i < sortingList.length ; i++) { // starts comparison after sorted partition.
+                if (sortingList[i] < sortingList[currentMinimum]){
+                    currentMinimum = i;
+                }
+            }
+            if (currentMinimum != x){ //if current minimum != original current minimum
+                swap(currentMinimum, x);
+            }
+        }
     }
 }
